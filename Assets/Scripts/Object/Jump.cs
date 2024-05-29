a188655db122b6cd495f99b73fe3f683d8b65045
rlaehgus0226 <rlaehgus0226.dev@gmail.com>
2024-05-29 16:59:19 +0900

    Add. 개인 과제 1차 완성
    .

---
+++ Assets/Scripts/Object/Jump.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float jumpPower = 0f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Rigidbody playerRigidbody = other.GetComponent<Rigidbody>();
+            if (playerRigidbody != null)
+            {
+                playerRigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/Object/JumpCollsion.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpCollsion : MonoBehaviour
+{
+    public float jumpForce = 10f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rb = collision.collider.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
